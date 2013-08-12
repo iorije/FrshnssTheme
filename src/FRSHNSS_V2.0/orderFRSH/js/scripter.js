a0fddbadcd741bdf09f3affd9c76bb4866e32253
iorije <isai.orye@gmail.com>
2013-08-12 23:27:07 +0200

    Responsive IPHONE, browser comp.
    layout breaks between 768px and large screen

--- src/FRSHNSS_V2.0/orderFRSH/js/scripter.js
+++ src/FRSHNSS_V2.0/orderFRSH/js/scripter.js
@@ -17,6 +17,27 @@
         $('.dynamic-div').each(function () {
             SetDivHeight($(this));
         });
+
+        //Firefox specific code
+        var FF = !(window.mozInnerScreenX == null);
+
+        if (FF) {
+            $('.training p').css('font-size', '39px');
+            $('a.button.fb-button').css('width', '155px');
+            if ($(this).width() <= 768) {
+                $('.trainingInfo p').css('font-size', '22.8px');
+            }
+        };
+
+        //IE specific code
+        var IE = /*@cc_on!@*/false;
+
+        if (IE) {
+            if ($(this).width() <= 768) {
+                $('.trainingInfo p').css('font-size', '22.8px');
+            }
+            $('a.button.fb-button').css('width', '156px');
+        }
     });
 
     $(window).resize(function () {
@@ -24,5 +45,27 @@
         $('.dynamic-div').each(function () {
             SetDivHeight($(this));
         });
+
+        //Firefox specific code
+        var FF = !(window.mozInnerScreenX == null);
+
+        if (FF) {
+            if ($(this).width() <= 768) {
+                $('.trainingInfo p').css('font-size', '22.8px');
+            } else {
+                $('.trainingInfo p').css('font-size', '30.5px');
+            }
+        };
+
+        //IE specific code
+        var IE = /*@cc_on!@*/false;
+
+        if (IE) {
+            if ($(this).width() <= 768) {
+                $('.trainingInfo p').css('font-size', '22.8px');
+            } else {
+                $('.trainingInfo p').css('font-size', '30.5px');
+            }
+        };
     });
 });
